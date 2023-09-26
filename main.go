bb7a4bbbaf78621a172baecbd24d9e890691795f
seipan <yamasakipann0218@gmail.com>
2023-09-27 01:38:56 +0900

    feat : log formatter & zerolog newlogger

--- main.go
+++ main.go
@@ -1,1 +1,22 @@
 package log
+
+import (
+	"strings"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+type LogFormatter struct {
+	LogLevel       string
+	LevelFieldName string
+
+	JSON            bool
+	TimeFieldFormat string
+	TimeFieldName   string
+}
+
+func NewLogger(serviceName string, opts ...LogFormatter) zerolog.Logger {
+	logger := log.With().Str("service", strings.ToLower(serviceName))
+	return logger.Logger()
+}
